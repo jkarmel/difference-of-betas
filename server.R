cea8cbe8c0382edc8a07cc5f5d6e4fb064abc95e
Jeremy Karmel <jeremy@hired.com>
2014-09-08 10:33:44 -0700

    don't try to plot hundreds of thousands of samples

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ library(scales)
 
 shinyServer(function(input, output) {
 
-  samples = 10^6
+  samples = reactive({strtoi(input$samples)})
   difference.of.betas = reactive({
     successes.A = strtoi(input$conversions.A)
     successes.B = strtoi(input$conversions.B)
@@ -20,19 +20,19 @@ shinyServer(function(input, output) {
     failures.B = n.B - successes.B
     prior.params.A = c(1,1)
     prior.params.B = c(1,1)
-    beta.A = rbeta(samples, successes.A + prior.params.A[1], failures.A + prior.params.A[2])
-    beta.B = rbeta(samples, successes.B + prior.params.B[1], failures.B + prior.params.B[2])
+    beta.A = rbeta(samples(), successes.A + prior.params.A[1], failures.A + prior.params.A[2])
+    beta.B = rbeta(samples(), successes.B + prior.params.B[1], failures.B + prior.params.B[2])
     sort(beta.A - beta.B)
   })
   
   output$proportion.A.bigger.than.B = renderText({
     times.A.bigger.than.B = sum(difference.of.betas() > 0)
-    percent(times.A.bigger.than.B/samples)
+    percent(times.A.bigger.than.B/samples())
   })
   
   output$table = renderDataTable({
     percentiles = c(.01, .05, .1, .25, .5, .75, .90, .95, .99)
-    chance.at.point = difference.of.betas()[percentiles * samples]
+    chance.at.point = difference.of.betas()[percentiles * samples()]
     
     proportion.B =  strtoi(input$conversions.B) /  strtoi(input$visitors.B)
     improvement =  chance.at.point/ proportion.B
@@ -43,8 +43,14 @@ shinyServer(function(input, output) {
   })
   
   output$A.less.B.plot = renderPlot({
-    plot(difference.of.betas(), seq(1, samples), xlab='n', yaxt = 'n', ylab = '')
-    axis(2,at = seq(0,samples, length=5), labels = percent(seq(0, 1, length=5)))
+    print(input$showPlot)
+    if (input$showPlot == TRUE && samples() < 10^6) {
+      plot(difference.of.betas(), seq(1, samples()), xlab='n', yaxt = 'n', ylab = '')
+      axis(2,at = seq(0,samples(), length=5), labels = percent(seq(0, 1, length=5)))  
+    } else {
+      plot(seq(1:5))
+    }
+    
   })
 
 })
--- ui.R
+++ ui.R
@@ -1,4 +1,3 @@
-
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
@@ -18,7 +17,9 @@ shinyUI(fluidPage(
       textInput('visitors.A', label = 'Visitors A', 100),
       textInput('visitors.B', label = 'Visitors B', 100),
       textInput('conversions.A', label = 'Conversions A', 50),
-      textInput('conversions.B', label = 'Conversions B', 50)
+      textInput('conversions.B', label = 'Conversions B', 50),
+      textInput('samples', label = 'Samples', '100000'),
+      checkboxInput(inputId = 'showPlot', label = 'Show Plot', value = FALSE)
     ),
     # Show a plot of the generated distribution
     mainPanel(
@@ -26,7 +27,10 @@ shinyUI(fluidPage(
       h3('chance a is bigger than b'),
       textOutput('proportion.A.bigger.than.B'),
       dataTableOutput('table'),
-      plotOutput('A.less.B.plot', height = 600)
+      conditionalPanel(condition = 'input.showPlot == true && input.samples < 100000',
+                       plotOutput('A.less.B.plot', height = 600)),
+      conditionalPanel(condition = 'input.showPlot == true && input.samples >= 100000',
+                       h3("can't show plot when samples are over 100,000"))
     )
   )
 ))
